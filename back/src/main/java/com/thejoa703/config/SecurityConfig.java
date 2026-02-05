c599773ed13532ccf9f0cfb3b035a7995fe2e4ea
remonadd21 <remonadd@gmail.com>
2026-02-05 11:12:07 +0900

    backend

--- back/src/main/java/com/thejoa703/config/SecurityConfig.java
+++ back/src/main/java/com/thejoa703/config/SecurityConfig.java
@@ -96,7 +96,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
  
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));  //★ Front 포트번호
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://15.134.37.205"));  //★ Front 포트번호
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
